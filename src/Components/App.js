0a0249aed6a4e906bf0241940074f944de2537e1
ikkiwar <you@example.com>
2020-03-17 22:22:03 -0600

    commit de prueba

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Login from '../Pages/Login';
+import panel from '../Pages/panel';
 
 function App(){
 return(
     <BrowserRouter>
     <Switch>
+    <Route exact path="/Panel" component={panel}></Route>
         <Route component={Login}></Route>
     </Switch>
     </BrowserRouter>
---
+++ src/Pages/panel.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class panel extends React.Component {
+    render() {
+return(
+    <div>
+        <h1>Holi</h1>
+    </div>
+)
+    }
+
+}
+export default panel;
